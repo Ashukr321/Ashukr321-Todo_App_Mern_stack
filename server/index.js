d0521d6f753573eade6c22975eb3880c4fdccfed
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-12-12 18:29:29 +0530

    add global error handler

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import { config } from 'dotenv';
 import helmet from 'helmet';
 import connectDb from './config/connectDatabase.js';
 import cors from 'cors';
+import globalErrorHandler from './middleware/globalErrorHandler.js';
+import { Error } from 'mongoose';
+
 // Load environment variables from .env file
 config({ path: './.env' });
 
@@ -21,7 +24,6 @@ Enables all origins ('*') and allows GET, POST, PUT, DELETE methods
 This middleware handles preflight requests and sets appropriate CORS headers */
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 
 
@@ -40,11 +42,16 @@ app.use(express.json());
 
 
 // Routes 
-app.get('/', (req, res) => {
- 
-  res.status(200).json({message:"success",data:req.body});
+
+app.get('/',  (req, res, next) => {
+  const err = new Error();
+  err.message = 'server side error';
+  err.statusCode = 400;
+  return next(err);
 });
 
+//  error handling middleware
+app.use(globalErrorHandler);
 // Define the port
 // process is the global object 
 const port = process.env.PORT;
