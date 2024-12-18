f73ebeca09226c2595caa09b0727575b2def6918
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2024-12-19 00:02:54 +0530

    delete all task

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -2,16 +2,17 @@ import express from 'express';
 const router = express.Router();
 import protect from '../middleware/protectRouteHandler.js';
 
-import { createTask,getTasks,updateTask } from '../controllers/taskController.js';
+import { createTask,getTasks,updateTask,deleteTask,deleteAllTasks } from '../controllers/taskController.js';
 
 
 
 // create task 
 router.post('/createTask',protect,createTask);
-router.post('/allTasks',protect,getTasks);
-router.get('/update/:productId',protect,updateTask);
-// read tasks 
-// update task 
-// delete task
+router.get('/allTasks',protect,getTasks);
+router.put('/update/:productId',protect,updateTask);
+router.delete('/delete/:taskId',protect,deleteTask);
+router.delete('/deleteTasks',protect,deleteAllTasks);
+
+
 
 export default router;
